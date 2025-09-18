be5967e93e25af30214d4fb4faee4ab4a6704858
Sam <633856+SamP20@users.noreply.github.com>
2025-09-18 17:53:17 +0100

    Add expiry date selector (#11)
    * Add expiry date selector
    
    * Remove unused imports

--- hackspace_storage/main/views.py
+++ hackspace_storage/main/views.py
@@ -1,13 +1,16 @@
-from datetime import timedelta
+from datetime import timedelta, date
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for, abort, current_app
 import secrets
+from flask_wtf import FlaskForm
 import sqlalchemy as sa
 from sqlalchemy.orm import joinedload
+from wtforms import BooleanField, DateField, TextAreaField, ValidationError
+from wtforms.validators import DataRequired, InputRequired
 
 from hackspace_storage.booking_rules import BookingError, try_make_booking, extend_booking
 from hackspace_storage.mailer import send_email
 
-from .forms import BookingForm, DeleteConfirmForm
+from .forms import DeleteConfirmForm
 from hackspace_storage.extensions import db
 from hackspace_storage.login import login_required
 from hackspace_storage.models import Area, Slot, User, Booking
@@ -36,7 +39,32 @@ def finish():
 def book_slot(slot_id: int):
     slot = db.get_or_404(Slot, slot_id)
 
-    form = BookingForm()
+    today = date.today()
+    max_booking= today+timedelta(days=slot.area.category.initial_duration_days)
+
+
+    class BookingForm(FlaskForm):
+        description = TextAreaField(
+            'Project description',
+            validators=[DataRequired()],
+            render_kw={"placeholder": "Brief description of project you are working on"}
+        )
+        expiry_date = DateField(
+            "Expiry date",
+            validators=[InputRequired()],
+            render_kw={
+                'min': (today+timedelta(days=1)).strftime("%Y-%m-%d"),
+                'max': (max_booking).strftime("%Y-%m-%d"),
+            }
+        )
+        remind_me = BooleanField("Send email reminders", default=True)
+
+        def validate_expiry_date(self, field: DateField):
+            selected: date = field.data  # pyright: ignore[reportAssignmentType]
+            if selected < today+timedelta(days=1) or selected > max_booking:
+                raise ValidationError("Expiry outside allowable range")
+
+    form = BookingForm(expiry_date=max_booking)
 
     if form.validate_on_submit():
         try:
@@ -45,11 +73,18 @@ def book_slot(slot_id: int):
                 slot,
                 form.description.data or "",
                 form.remind_me.data,
-            ) # pyright: ignore[reportArgumentType]
+                form.expiry_date.data, # pyright: ignore[reportArgumentType]
+            )
             flash(f"Booking success", 'success')
 
             if booking.remind_me:
                 reminder_date = booking.expiry - timedelta(days=slot.area.category.extension_period_days)
+                if reminder_date <= date.today():
+                    # If reminder date is in the past or today then skip sending it
+                    reminder_date = None
+                    booking.reminder_sent = True
+                    db.session.commit()
+
                 send_email(
                     g.user,
                     "email/slot_booked",
@@ -110,7 +145,7 @@ def extend(booking_id: int):
     booking = db.get_or_404(Booking, booking_id)
     if booking.user != g.user:
         abort(403)
-    
+
     try:
         extend_booking(booking)
         flash("Extension success", 'success')

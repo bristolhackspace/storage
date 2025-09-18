491e2214d6e26a022a94688b2342109203280f46
SamP20 <633856+SamP20@users.noreply.github.com>
2025-09-18 13:51:43 +0200

    Remove unused imports

--- hackspace_storage/main/forms.py
+++ hackspace_storage/main/forms.py
@@ -1,6 +1,4 @@
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, BooleanField, IntegerField
-from wtforms.validators import DataRequired
 
 class DeleteConfirmForm(FlaskForm):
     pass

89647b3e59affb37271491915c158c1b79c843df
SamP20 <633856+SamP20@users.noreply.github.com>
2025-09-18 13:47:33 +0200

    Add expiry date selector

--- hackspace_storage/main/forms.py
+++ hackspace_storage/main/forms.py
@@ -1,16 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, BooleanField
+from wtforms import TextAreaField, BooleanField, IntegerField
 from wtforms.validators import DataRequired
-
-from hackspace_storage.models import Slot, User
-
-class BookingForm(FlaskForm):
-    description = TextAreaField(
-        'Project description',
-        validators=[DataRequired()],
-        render_kw={"placeholder": "Brief description of project you are working on"}
-    )
-    remind_me = BooleanField("Send email reminders", default=True)
 
 class DeleteConfirmForm(FlaskForm):
     pass

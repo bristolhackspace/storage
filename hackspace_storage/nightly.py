112f504bd61585230096ca7520dd04cca49ec1ea
Sam <633856+SamP20@users.noreply.github.com>
2025-10-22 22:39:29 +0100

    Enable booking deletion and reminders (#30)

--- hackspace_storage/nightly.py
+++ hackspace_storage/nightly.py
@@ -25,29 +25,28 @@ def nightly(dry_run: bool):
                 print(f"Expiry reminder for booking {booking.description} with expiry {booking.expiry} from slot {booking.slot.name}. Booked by {booking.user.name}")
             else:
                 pass
-                # booking.reminder_sent = True
-                # db.session.commit()
+                booking.reminder_sent = True
+                db.session.commit()
 
-                # send_email(
-                #     booking.user,
-                #     "email/expiry_reminder",
-                #     subject="Booking expiry reminder",
-                #     slot=booking.slot,
-                #     booking=booking,
-                # )
+                send_email(
+                    booking.user,
+                    "email/expiry_reminder",
+                    subject="Booking expiry reminder",
+                    slot=booking.slot,
+                    booking=booking,
+                )
 
         if today > booking.expiry:
             if dry_run:
                 print(f"Delete booking {booking.description} with expiry {booking.expiry} from slot {booking.slot.name}. Booked by {booking.user.name}")
             else:
-                pass
-                # db.session.delete(booking)
-                # db.session.commit()
+                db.session.delete(booking)
+                db.session.commit()
 
-                # send_email(
-                #     booking.user,
-                #     "email/expired",
-                #     subject="Booking expired",
-                #     slot=booking.slot,
-                #     booking=booking
-                # )
+                send_email(
+                    booking.user,
+                    "email/expired",
+                    subject="Booking expired",
+                    slot=booking.slot,
+                    booking=booking
+                )
